cb43dc2ec8a284935fb779ba3abc3b0948185c1e
TheSebitzu <sebibonea@gmail.com>
2025-02-13 01:34:21 +0200

    Change ServiceWorker

---
+++ common/index.js
@@ -0,0 +1,11 @@
+// Clear skipped exercises when loading the index page
+localStorage.removeItem('skippedExercises');
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/gym-app-ai/common/sw.js').then(function(registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function(error) {
+            console.log('ServiceWorker registration failed: ', error);
+        });
+    });
+}
--- common/sw.js
+++ common/sw.js
@@ -1,7 +1,51 @@
+// service-worker.js
+
+// Cache version
+const CACHE_NAME = 'v1';
+
+// Files to cache
+const CACHE_ASSETS = [
+  '/gym-app-ai/common/workout.css',
+  '/gym-app-ai/common/manifest.json',
+  '/gym-app-ai/common/index.js',
+  '/gym-app-ai/common/icon-192x192.png',
+  '/gym-app-ai/common/icon-512x512.png',
+  '/gym-app-ai/ex.js',
+  '/gym-app-ai/index.html',
+];
+
+// Install event
 self.addEventListener('install', event => {
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching files...');
+        return cache.addAll(CACHE_ASSETS);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
+// Activate event
+self.addEventListener('activate', event => {
+  // Remove old caches
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache...');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event
 self.addEventListener('fetch', event => {
-    event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
